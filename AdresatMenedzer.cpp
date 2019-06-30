4b75006ee1315e71b7f9c4f7106957307d033bb6
Mateusz Paczwa <mateusz.paczwa.programista@gmail.com>
2019-06-30 15:03:09 +0200

    Added option - addressee add

---
+++ AdresatMenedzer.cpp
@@ -0,0 +1,82 @@
+#include "AdresatMenedzer.h"
+#include "MetodyPomocnicze.h"
+//#include "PlikZUzytkownikami.h"
+
+void AdresatMenedzer::dodajAdresata(int idZalogowanegoUzytkownika)
+{
+    Adresat adresat;
+
+    system("cls");
+    cout << " >>> DODAWANIE NOWEGO ADRESATA <<<" << endl << endl;
+    adresat = podajDaneNowegoAdresata(idZalogowanegoUzytkownika);
+
+    adresaci.push_back(adresat);
+    //dopiszAdresataDoPliku(adresat);
+
+    //return ++idOstatniegoAdresata;
+}
+
+Adresat AdresatMenedzer::podajDaneNowegoAdresata(int idZalogowanegoUzytkownika)
+{
+    Adresat adresat;
+    string imie, nazwisko, numerTelefonu, email, adres;
+
+    adresat.ustawID(++idOstatniegoAdresata);
+    adresat.ustawIDUzytkownika(idZalogowanegoUzytkownika);
+
+    cout << "Podaj imie: ";
+    cin >> imie;
+    adresat.ustawImie(imie);
+    adresat.ustawImie(MetodyPomocnicze::zamienPierwszaLitereNaDuzaAPozostaleNaMale(imie));
+
+    cout << "Podaj nazwisko: ";
+    cin >> nazwisko;
+    adresat.ustawNazwisko(nazwisko);
+    adresat.ustawNazwisko(MetodyPomocnicze::zamienPierwszaLitereNaDuzaAPozostaleNaMale(nazwisko));
+
+    cout << "Podaj numer telefonu: ";
+    cin.sync();
+    getline(cin, numerTelefonu);
+    adresat.ustawNumerTelefonu(numerTelefonu);
+
+    cout << "Podaj email: ";
+    cin >> email;
+    adresat.ustawEmail(email);
+
+    cout << "Podaj adres: ";
+    cin.sync();
+    getline(cin, adres);
+    adresat.ustawAdres(adres);
+
+    return adresat;
+}
+
+void AdresatMenedzer::wyswietlWszystkichAdresatow()
+{
+    system("cls");
+    if (!adresaci.empty())
+    {
+        cout << "             >>> ADRESACI <<<" << endl;
+        cout << "-----------------------------------------------" << endl;
+        for (vector <Adresat> :: iterator itr = adresaci.begin(); itr != adresaci.end(); itr++)
+        {
+            wyswietlDaneAdresata(*itr);
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << endl << "Ksiazka adresowa jest pusta." << endl << endl;
+    }
+    system("pause");
+}
+
+void AdresatMenedzer::wyswietlDaneAdresata(Adresat adresat)
+{
+    cout << endl << "Id:                 " << adresat.pobierzID() << endl;
+    cout << "Imie:               " << adresat.pobierzImie() << endl;
+    cout << "Nazwisko:           " << adresat.pobierzNazwisko() << endl;
+    cout << "Numer telefonu:     " << adresat.pobierzNumerTelefonu() << endl;
+    cout << "Email:              " << adresat.pobierzEmail() << endl;
+    cout << "Adres:              " << adresat.pobierzAdres() << endl;
+}
--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,12 +1,14 @@
 #include <iostream>
 
 #include "UzytkownikMenedzer.h"
+#include "AdresatMenedzer.h"
 
 using namespace std;
 
 class KsiazkaAdresowa
 {
     UzytkownikMenedzer uzytkownikMenedzer;
+    AdresatMenedzer adresatMenedzer;
 
 public:
     KsiazkaAdresowa(string nazwaPlikuZUzytkownikami) : uzytkownikMenedzer(nazwaPlikuZUzytkownikami){
@@ -18,4 +20,8 @@ public:
     void logowanieUzytkownika();
     void zmianaHaslaZalogowanegoUzytkownika();
     void wylogowywanieUzytkownika();
+    void dodajAdresata();
+    void wyswietlWszystkichAdresatow();
+
+
 };
